cce1f2fe6f9ac88b603bd76c9ee76f65dc2c61a7
huntzhan <programmer.zhx@gmail.com>
2016-06-08 17:11:07 +0800

    init.

---
+++ http_rsync_toolset/metadata.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    division, absolute_import, print_function, unicode_literals,
+)
+
+
+NAME = 'http_rsync_toolset'
+VERSION = '0.1.0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'setting up rsync daemon and synchronize file/directory.'
