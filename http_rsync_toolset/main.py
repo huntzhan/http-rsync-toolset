6b099048bdbb8299856b462b435d7bb1ab8a20e3
huntzhan <programmer.zhx@gmail.com>
2016-06-12 09:57:58 +0800

    Fixed package structure.

--- http_rsync_toolset/main.py
+++ http_rsync_toolset/main.py
@@ -5,7 +5,24 @@ from __future__ import (
 from builtins import *                  # noqa
 from future.builtins.disabled import *  # noqa
 
+from os import getcwd
+from os.path import dirname, join
+from cpl import replace
+
 
 def entry_point():
     print("entry_point")
     return 42
+
+
+def deploy():
+    TARGET_NAME = 'rsync-playbook'
+    src = join(
+        dirname(__file__),
+        TARGET_NAME,
+    )
+    dst = join(
+        getcwd(),
+        TARGET_NAME,
+    )
+    replace(src, dst)
